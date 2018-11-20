69283a3f1befe18e9d87f2fb9d6c31a2d813f3d0
Alexander Veats <ajveats@gmail.com>
2018-11-20 16:38:39 +0000

    Changed all of the topic quiz pages by rearranging elements. Started on the quiz javascript files, created AnswerCheck() and GenerateUserScorePercentage()

--- resources/client/js/topic1Quiz.js
+++ resources/client/js/topic1Quiz.js
@@ -1,3 +1,16 @@
+var userAnswer = document.getElementById("userAnswer").value;
+var answerCorrect = a;
+var userScore = 0;
 function pageLoad() {
 
 }
+function AnswerCheck(){
+    if(userAnswer.toLowerCase() == answerCorrect ){
+        userScore = userAnswer + 1
+    }
+}
+function GenerateUserScorePecentage(){
+    userScorePercentage = userScore * 10;
+}
+resetLoginForm()
+resetNewUserForm()
