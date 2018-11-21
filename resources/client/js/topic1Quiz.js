6f422ff93cea78db5690069c661eccdfbbcbc246
Alexander <40860289+AJVEATS@users.noreply.github.com>
2018-11-21 13:47:15 +0000

    Added more functions to topic1Quiz.js page
    Added more functions to topic1Quiz.js page

--- resources/client/js/topic1Quiz.js
+++ resources/client/js/topic1Quiz.js
@@ -1,16 +1,39 @@
 var userAnswer = document.getElementById("userAnswer").value;
 var answerCorrect = a;
 var userScore = 0;
+var questionNumber = 0
 function pageLoad() {
 
 }
+function nextQuestion(){
+    while (questionNumber <= 9 && questionNumber >= 0){
+
+    }
+}
+function AnswerWrong(){
+    userScore == userScore + 1
+    nextQuestion()
+    window.alert("That is the correct answer. The correct answer is" + answerCorrect)
+}
+function AnswerCorrect(){
+    userScore == userScore + 1
+    nextQuestion()
+    window.alert("That is the correct answer.")
+}
 function AnswerCheck(){
     if(userAnswer.toLowerCase() == answerCorrect ){
-        userScore = userAnswer + 1
+        AnswerCorrect()
+    }else{
+        AnswerWrong()
     }
 }
 function GenerateUserScorePercentage(){
-    let userScorePercentage = userScore * 10;
+    if( userScore > 0){
+        var userScorePercentage = ((userScore*10)+"%")
+    }else{
+        return "0%"
+    }
 }
-resetLoginForm()
-resetNewUserForm()
+function updateUserScore(){
+    
+}
