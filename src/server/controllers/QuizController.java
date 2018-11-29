0111cd5de508a14372aed665244f42e9f9b7bd11
Alexander Veats <aveats.76709@farnborough.ac.uk>
2018-11-29 15:04:57 +0000

    server is now working by removing the broken java code in the QuizController class and removing UserQuizController

--- src/server/controllers/QuizController.java
+++ src/server/controllers/QuizController.java
@@ -1,48 +1,6 @@
 package server.controllers;
-import server.models.Quiz;
-import server.models.services.QuizService;
-import javax.ws.rs.*;
-import javax.ws.rs.core.Cookie;
-import javax.ws.rs.core.MediaType;
 
-import static server.Logger.log;
-
-@Path("quiz/")
 public class QuizController {
-    String AnswerCorrect = "A";
-    @GET
-    @Path("answer")
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces(MediaType.TEXT_PLAIN)
-    public Character getAnswer(@FormParam("answerCorrect") String AnswerCorrect){
-        log("/quiz/answer - Get" + AnswerCorrect);
-        QuizService.selectAllInto(Quiz.quizs);
-
-        return null;
-    }
-    public int userScore = 0;
-    public String AnswerCheck(){
-        String userAnswer = "a";
-
-
-        if (userAnswer.toUpperCase() == AnswerCorrect){
-            userScore = userScore + 1;
-        }
-        return null;
-    }
-    public void GenerateUserScorePecentage(){
-        int userScorePercentage = ( userScore * 10);
-
-    }
-    @GET
-    @Path("get")
-    @Produces(MediaType.TEXT_PLAIN)
-    public void getAnswerCorrect(@CookieParam("sessionToken")Cookie sessionCookie){
-        Character AnswerCorrect = QuizService.validateSessionCookie(sessionCookie);
-        Character UserAnswer = null;
-        if (AnswerCorrect == UserAnswer){
-
-        }
-    }
-
 }
+
+
