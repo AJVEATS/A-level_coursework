8036ab5bc2974121fa99f06f06d2426686e4ed5c
Alexander <ajveats@gmail.com>
2019-01-04 13:11:07 +0000

    Finished all comment for the quizQuestion files: html, js, model, controller and service files to do annotation of code in coursework tracker.

--- resources/client/js/quiz.js
+++ resources/client/js/quiz.js
@@ -1,36 +1,36 @@
-function pageLoad(){
-    getQuizQuestions();
+function pageLoad(){    // New function for the web page that is meant to run upon the page loading.
+    getQuizQuestions();    // When the function pageLoad() it calls upon the getQuizQuestion() to run also.
 }
 
-function getQuizQuestions(){
-    console.log("Invoked getQuizQuestions()");
-    var quizId = sessionStorage.getItem('quizId');
+function getQuizQuestions(){   // New function declared called getQuizQuestions(). It is called upon in the pageLoad() function.
+    console.log("Invoked getQuizQuestions()");    // Outputs "Invoked getQuizQuestions" into the browser console/
+    var quizId = sessionStorage.getItem('quizId');    // New variable quizID is declared and is set to the same value as the quizId in the session storage.
 
     $.ajax({
         type: "GET",
-        url: "/quizquestion/list",
-        data:  {'quizId' : quizId},
+        url: "/quizquestion/list",    // The API path for the list function in the quizQuestionController.
+        data:  {'quizId' : quizId},    // The data that is being collected is quizId.
         success: response => {
-            if (response.toString().startsWith('Error:')){
-                alert(response);
-            } else {
-                formatQuizQuestionList(response);
+            if (response.toString().startsWith('Error:')){    // If the response from the server starts with an error, it runs the if statement.
+                alert(response);    // If the if statement is run it returns the response from the server.
+            } else {    // If the response from the server does not start with and "Error" it runs the next part of the statement.
+                formatQuizQuestionList(response);    // It runs teh function formatQuizQuestionList() function from below with the response from the server.
             }
         }
     });
 }
 
-function formatQuizQuestionList(data){
-    console.log(data);
-    let dataHTML = '<tr><td>Question</td>' +
-        '<td>|   Answer A   |</td>' +
-        '<td>|   Answer B   |</td>' +
-        '<td>|   Answer C   |</td>' +
-        '<td>|   Answer D   |</td>' +
-        '<td>|   Your Answer   |</td>' +
+function formatQuizQuestionList(data){    // New function declared called formatQuizQuestionList() with imported data from the API path.
+    console.log(data);    // The server logs the data from the server into the web browser with the data collected from the database.
+    let dataHTML = '<tr><td>Question</td>' +    // Formatting how the data will be outputted onto the web page.
+        '<td>Answer A</td>' +
+        '<td>Answer B</td>' +
+        '<td>Answer C</td>' +
+        '<td>Answer D</td>' +
+        '<td>Your Answer</td>' +
         '</tr>';
     for (let item of data){
-        dataHTML += `<tr>\<td>${item.question}</td>\` +
+        dataHTML += `<tr>\<td>${item.question}</td>\` +    // The name of the data that is being collected from the database.
              <td>${item.answerA}</td>` +
             `<td>${item.answerB}</td>` +
             `<td>${item.answerC}</td>` +
@@ -40,6 +40,6 @@ function formatQuizQuestionList(data){
     $('#QuestionList').html(dataHTML);
 }
 
-function checkUserAnswer() {
+function checkUserAnswer() {    // New function declared called checkUserAnswer() which is to check if the users answer is the same as the questions correct answer.
     let usersAnswer = document.getElementById("userAnswer");
 }
--- src/server/controllers/QuizQuestionController.java
+++ src/server/controllers/QuizQuestionController.java
@@ -34,10 +34,10 @@ public class QuizQuestionController {
             return response.toString();    // Returns the response that was declared above in the format of a string.
         }
 
-        //now we have the quizQuestions records back we need to get the actual question referred to by the questionID
+        //  now we have the quizQuestions records back we need to get the actual question referred to by the questionID
         status = QuestionService.selectAllInto(Question.questions);    // Status is declared.
         if (!status.equals("OK")) {    // If the status is not equal to ok it will run teh code inside of the if statement.
-            JSONObject response = new JSONObject();
+            JSONObject response = new JSONObject();    // A new JSONObject is declared
             response.put("Error: ", status);    // Declares the response as an error and add the current status.
             return response.toString();    // Returns the response that was declared above in the format of a string.
         }
@@ -51,15 +51,15 @@ public class QuizQuestionController {
             for (Question q : Question.questions) {
                 if(qq.getQuestionId()==q.getQuestionId()) {
                     JSONObject j = new JSONObject();
-                    j.put("questionID", q.getQuestionId());
-                    j.put("question", q.getQuestion());
-                    j.put("answerA", q.getAnswerA());
-                    j.put("answerB", q.getAnswerB());
-                    j.put("answerC", q.getAnswerC());
-                    j.put("answerD", q.getAnswerD());
-                    j.put("answerCorrect", q.getAnswerCorrect());
+                    j.put("questionID", q.getQuestionId());    // Adds the rows questionId to the JSONArray from the database.
+                    j.put("question", q.getQuestion());   // Adds the rows question to the JSONArray from the database.
+                    j.put("answerA", q.getAnswerA());    // Adds the rows answerA to the JSONArray from the database.
+                    j.put("answerB", q.getAnswerB());    // Adds the rows answerB to the JSONArray from the database.
+                    j.put("answerC", q.getAnswerC());   // Adds the rows answerC to the JSONArray from the database.
+                    j.put("answerD", q.getAnswerD());    // Adds the rows answerD to the JSONArray from the database.
+                    j.put("answerCorrect", q.getAnswerCorrect());    // Adds the rows answerCorrect to the JSONArray from the database.
 
-                    newJSONArray.add(j);
+                    newJSONArray.add(j);    // It goes onto the next row of the database.
                 }
             }
         }
